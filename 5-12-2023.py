f28f946ff43ebbe8d82e14b1bfdb025abd641f68
shazil20 <shazil03144426622@gmail.com>
2023-12-05 05:40:39 -0800

    first commit

---
+++ 5-12-2023.py
@@ -0,0 +1,67 @@
+a = int(input("enter : "))
+if(a>18):
+    print("ok")
+else:
+    print("no")
+a = [1,2,3,4,5,6]
+for i in range(20):
+    print(i + 2)
+a = int(12)
+print(a)
+a = int(inpkut("enter any no. :"))
+if(a>18):
+    print("you can drive")
+else:
+    print("no")
+a = "hello world"
+for x in a:
+    print(x)
+i = range(10)
+for x in i:
+    print(x)
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "i" in x:
+    newlist.append(x)
+
+print(newlist)
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+a = 123
+print(a,type(a))
+a = 113
+if(a==12):
+    print("Is Equal")
+# elif(a>11):
+#     print("True")
+else:
+    print("No")
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+a = range(10)
+for x in a:
+    print(x)
+a = {"a","b","c"}
+b = {"c","d","e"}
+a.update(b)
+print(a)
+a = {
+    "BMW" : "M5",
+    "Bughatti" : "Divo",
+    "Lambo" : "Hurrican"
+}
+for x in a:
+    print(a)
+print(len(a))
+a = input(int("enter any no. :"))
+
+
